8ad1e2c0a2df788899398d037459d5835f1bc7e3
Germán Martín <github@gmartin.net>
2026-04-11 16:27:56 +0200

    chore: remove ABI splits from build.gradle.kts and update commit references in F-Droid metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -138,16 +138,6 @@ android {
         }
     }
 
-
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = false
-        }
-    }
-
     dependenciesInfo {
         // Disables dependency metadata when building APKs (for IzzyOnDroid/F-Droid)
         includeInApk = false

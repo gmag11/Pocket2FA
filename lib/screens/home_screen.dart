c592fb8743560792197c01040be351b30048ec28
Germán Martín <github@gmartin.net>
2026-04-26 19:24:24 +0200

    chore: update version to 0.9.10

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -620,7 +620,7 @@ class _HomePageState extends State<HomePage>
 
   void _showAboutDialog() {
     // NOTE: appVersion is kept in sync manually with pubspec.yaml's version: field.
-    const appVersion = '0.9.9';
+    const appVersion = '0.9.10';
 
     showDialog<void>(
       context: context,
